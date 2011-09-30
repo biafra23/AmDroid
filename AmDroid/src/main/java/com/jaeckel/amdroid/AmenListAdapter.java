55b2830d2d015287c41c991509c900a895991d61
Dirk Jäckel <github@dirk.jaeckel.name>
2011-09-30 02:09:46 +0200

    build statements

--- AmDroid/src/main/java/com/jaeckel/amdroid/AmenListAdapter.java
+++ AmDroid/src/main/java/com/jaeckel/amdroid/AmenListAdapter.java
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.jaeckel.amdroid.api.model.Amen;
+import com.jaeckel.amdroid.api.model.Statement;
 
 import java.util.List;
 
@@ -38,6 +39,7 @@ public class AmenListAdapter extends ArrayAdapter<Amen> {
     View row = convertView;
     Amen amen = getItem(position);
 
+    final Statement stmt = amen.getStatement();
     if (row == null) {
       row = inflater.inflate(R.layout.list_item_amen, parent, false);
       row.setTag(R.id.user, row.findViewById(R.id.user));
@@ -58,14 +60,66 @@ public class AmenListAdapter extends ArrayAdapter<Amen> {
       from = from + " disputes " + amen.getReferringAmen().getUser().getName();
     }
     user.setText(from);
+
     TextView statement = (TextView) row.getTag(R.id.statement);
 
-    String disputed ="";
+    String disputed = "";
     if (disputes) {
       disputed = " NOT ";
       disputed += amen.getReferringAmen().getStatement().getObjekt().getName();
     }
-    statement.setText(amen.getStatement().toDisplayString() + disputed);
+//    statement.setText(amen.getStatement().toDisplayString() + disputed);
+    StringBuilder statementBuilder = new StringBuilder();
+
+    statementBuilder
+      .append(stmt.getObjekt().getName())
+      .append(" ");
+
+    if (stmt.getObjekt().getKindId() == 2) {
+      //Thing
+      statementBuilder.append("is the ");
+      if (stmt.getTopic().isBest()) {
+        statementBuilder.append(" Best ");
+      } else {
+        statementBuilder.append(" Worst ");
+      }
+      statementBuilder
+        .append(stmt.getTopic().getDescription())
+        .append(" ")
+        .append(stmt.getTopic().getScope());
+
+
+    } else if (stmt.getObjekt().getKindId() == 1) {
+      //Place
+      statementBuilder.append("is the ");
+      if (stmt.getTopic().isBest()) {
+        statementBuilder.append(" Best Place for ");
+      } else {
+        statementBuilder.append(" Worst Place for ");
+      }
+      statementBuilder
+        .append(stmt.getTopic().getDescription())
+        .append(" ")
+        .append(stmt.getTopic().getScope());
+
+    } else if (stmt.getObjekt().getKindId() == 0) {
+      statementBuilder.append("is the ");
+      if (stmt.getTopic().isBest()) {
+        statementBuilder.append(" Best ");
+      } else {
+        statementBuilder.append(" Worst ");
+      }
+      statementBuilder
+        .append(stmt.getTopic().getDescription())
+        .append(" ")
+        .append(stmt.getTopic().getScope());
+
+    }
+
+    statementBuilder
+      .append(disputed);
+
+    statement.setText(statementBuilder.toString());
 
 
     ImageView userImage = (ImageView) row.getTag(R.id.user_image);
