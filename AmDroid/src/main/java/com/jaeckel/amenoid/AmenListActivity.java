df22f95f89156fe5b766d6fb901e769bf76ad444
Dirk Jäckel <github@dirk.jaeckel.name>
2011-11-09 22:46:40 +0100

    ignore clicks on "Please wait..."

--- AmDroid/src/main/java/com/jaeckel/amenoid/AmenListActivity.java
+++ AmDroid/src/main/java/com/jaeckel/amenoid/AmenListActivity.java
@@ -292,7 +292,7 @@ public class AmenListActivity extends ListActivity {
   @Override
   protected void onListItemClick(ListView l, View v, int position, long id) {
 
-    if (position > 0) {
+    if (position > 0 && position < getListAdapter().getCount()) {
 
       Amen amen = (Amen) getListAdapter().getItem(position - 1);
 
