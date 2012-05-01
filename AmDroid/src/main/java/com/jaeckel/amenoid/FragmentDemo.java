782c843f49ee2564f60075c6d59532361820dee4
Dirk Jäckel <github@dirk.jaeckel.name>
2012-05-01 18:02:17 +0200

    fragment example 2

--- AmDroid/src/main/java/com/jaeckel/amenoid/FragmentDemo.java
+++ AmDroid/src/main/java/com/jaeckel/amenoid/FragmentDemo.java
@@ -2,6 +2,10 @@ package com.jaeckel.amenoid;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Display;
+import android.view.WindowManager;
 
 /**
  * @author biafra
@@ -10,7 +14,48 @@ import android.support.v4.app.FragmentActivity;
 public class FragmentDemo extends FragmentActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+//
+//    FragmentManager fragmentManager = this.getFragmentManager();
+//    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//    WindowManager windowManager = getWindowManager();
+//    Display d = windowManager.getDefaultDisplay();
+//    if (d.getWidth() > d.getHeight()) {
+//      //landscape
+//      Fragment1 fragment1 = new Fragment1();
+//      fragmentTransaction.replace(android.R.id.content, fragment1);
+//    } else {
+//      //portrait
+//      Fragment2 fragment2 = new Fragment2();
+//      fragmentTransaction.replace(android.R.id.content, fragment2);
+//    }
+//    fragmentTransaction.commit();
 
     setContentView(R.layout.frag);
+
+
+  }
+
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    FragmentManager fragmentManager = this.getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+    WindowManager windowManager = getWindowManager();
+    Display d = windowManager.getDefaultDisplay();
+    if (d.getWidth() > d.getHeight()) {
+      //landscape
+      Fragment1 fragment1 = new Fragment1();
+      fragmentTransaction.replace(android.R.id.content, fragment1);
+    } else {
+      //portrait
+      Fragment2 fragment2 = new Fragment2();
+      fragmentTransaction.replace(android.R.id.content, fragment2);
+    }
+    fragmentTransaction.commit();
+
   }
 }
