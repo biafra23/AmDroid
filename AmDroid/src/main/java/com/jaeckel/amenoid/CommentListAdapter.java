fcd738a4672115760a3cbb5a3a50058119a161df
Dirk Jäckel <github@dirk.jaeckel.name>
2012-05-20 01:56:50 +0200

    delete comments in background

--- AmDroid/src/main/java/com/jaeckel/amenoid/CommentListAdapter.java
+++ AmDroid/src/main/java/com/jaeckel/amenoid/CommentListAdapter.java
@@ -3,11 +3,14 @@ package com.jaeckel.amenoid;
 import java.io.IOException;
 import java.util.List;
 
+import com.jaeckel.amenoid.api.model.Amen;
 import com.jaeckel.amenoid.api.model.Comment;
 import com.jaeckel.amenoid.app.AmenoidApp;
-
-import android.content.Context;
+import com.jaeckel.amenoid.util.AmenLibTask;
 import com.jaeckel.amenoid.util.Log;
+
+import android.app.Activity;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,9 +25,9 @@ import android.widget.TextView;
 public class CommentListAdapter extends ArrayAdapter<Comment> {
   private int            textViewResourceId;
   private LayoutInflater inflater;
+  private Activity        context;
 
-
-  public CommentListAdapter(Context context, int textViewResourceId, List<Comment> objects) {
+  public CommentListAdapter(Activity context, int textViewResourceId, List<Comment> objects) {
     super(context, textViewResourceId, objects);
     this.textViewResourceId = textViewResourceId;
     inflater = LayoutInflater.from(context);
@@ -75,13 +78,10 @@ public class CommentListAdapter extends ArrayAdapter<Comment> {
         public void onClick(View v) {
           Log.d("CommentListAdapter", "trash as trash can!!!: id: " + comment_id);
 
-          try {
 
-            AmenoidApp.getInstance().getService().deleteComment(comment_id);
+          new DeleteCommentTask(context).execute(comment_id);
 
-          } catch (IOException e) {
-            throw new RuntimeException(e);
-          }
+
         }
       });
     } else {
@@ -92,113 +92,35 @@ public class CommentListAdapter extends ArrayAdapter<Comment> {
   }
 
 
-//  //
-//    // DeleteCommentTask
-//    //
-//    private class DeleteCommentTask extends AmenLibTask<Long, Integer, Amen> {
-//
-//      public DeleteCommentTask(Activity context) {
-//        super(context);
-//      }
-//
-//      protected Amen wrappedDoInBackground(Long... commentIds) throws IOException {
-//
-//
-//        AmenoidApp.getInstance().getService().deleteComment(commentIds[0]);
-//
-//        Amen amen = service.getAmenForId(commentIds[0]);
-//        Log.d(TAG, "Amen returned from getAmenForId(): " + amen);
-//
-//
-//
-//
-//        return amen;
-//
-//      }
-//
-//      @Override
-//      protected void onPreExecute() {
-//      }
-//
-//      protected void wrappedOnPostExecute(Amen result) {
-//
-//        if (result != null) {
-//          currentAmen = result;
-//
-//          Log.d(TAG, "Current (NEW!) Amen: " + currentAmen);
-//          StringBuilder commentsText = new StringBuilder();
-//          //commentsText.append("Comment(s):\n");
-//
-//          for (Comment comment : currentAmen.getComments()) {
-//            Log.d(TAG, "comment: " + comment);
-//            commentsText.append(formatCommentDate(comment.getCreatedAt()));
-//            commentsText.append(": ");
-//            commentsText.append(comment.getUser().getName());
-//            commentsText.append(": ");
-//            commentsText.append(comment.getBody());
-//            commentsText.append("\n");
-//          }
-//          commentsTextView.setText(commentsText.toString());
-//
-//          setAmenButtonListener();
-//          currentStatement = result.getStatement();
-//          populateFormWithAmen(true);
-//          final List<User> users = currentStatement.getAgreeingNetwork();
-//          thumbs = new ThumbnailAdapter(AmenDetailActivity.this, new UserListAdapter(AmenDetailActivity.this, android.R.layout.activity_list_item, users), cache, IMAGE_IDS);
-//          setListAdapter(thumbs);
-//        }
-//      }
-//    }
-//  //
-//  // GetAmenTask
-//  //
-//  private class GetAmenTask extends AmenLibTask<Long, Integer, Amen> {
-//
-//    public GetAmenTask(Activity context) {
-//      super(context);
-//    }
-//
-//    protected Amen wrappedDoInBackground(Long... amenIds) throws IOException {
-//
-//      Amen amen = service.getAmenForId(amenIds[0]);
-//      Log.d(TAG, "Amen returned from getAmenForId(): " + amen);
-//
-//      return amen;
-//
-//    }
-//
-//    @Override
-//    protected void onPreExecute() {
-//    }
-//
-//    protected void wrappedOnPostExecute(Amen result) {
-//
-//      if (result != null) {
-//        currentAmen = result;
-//
-//        Log.d(TAG, "Current (NEW!) Amen: " + currentAmen);
-//        StringBuilder commentsText = new StringBuilder();
-//        //commentsText.append("Comment(s):\n");
-//
-//        for (Comment comment : currentAmen.getComments()) {
-//          Log.d(TAG, "comment: " + comment);
-//          commentsText.append(formatCommentDate(comment.getCreatedAt()));
-//          commentsText.append(": ");
-//          commentsText.append(comment.getUser().getName());
-//          commentsText.append(": ");
-//          commentsText.append(comment.getBody());
-//          commentsText.append("\n");
-//        }
-//        commentsTextView.setText(commentsText.toString());
-//
-//        setAmenButtonListener();
-//        currentStatement = result.getStatement();
-//        populateFormWithAmen(true);
-//        final List<User> users = currentStatement.getAgreeingNetwork();
-//        thumbs = new ThumbnailAdapter(AmenDetailActivity.this, new UserListAdapter(AmenDetailActivity.this, android.R.layout.activity_list_item, users), cache, IMAGE_IDS);
-//        setListAdapter(thumbs);
-//      }
-//    }
-//  }
+  //
+  // DeleteCommentTask
+  //
+  private class DeleteCommentTask extends AmenLibTask<Long, Integer, Amen> {
+
+    public DeleteCommentTask(Activity context) {
+      super(context);
+    }
+
+    protected Amen wrappedDoInBackground(Long... commentIds) throws IOException {
+
+      Amen amen = null;
+      for (Long commentId : commentIds) {
+        AmenoidApp.getInstance().getService().deleteComment(commentId);
+
+        amen = AmenoidApp.getInstance().getService().getAmenForId(commentIds[0]);
+        Log.d(TAG, "Amen returned from getAmenForId(): " + amen);
+
+      }
+
+      return amen;
+    }
+
+    @Override
+    protected void onPreExecute() {
+    }
+
+    protected void wrappedOnPostExecute(Amen result) {
+    }
+  }
 
 }
