e945e49084399729473fa067719c1a1e0800af8e
Dirk Jäckel <github@dirk.jaeckel.name>
2012-05-22 19:38:44 +0200

    fragmentizing

---
+++ AmDroid/src/main/java/com/jaeckel/amenoid/AmenListFragmentActivity.java
@@ -0,0 +1,20 @@
+package com.jaeckel.amenoid;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author biafra
+ * @date 5/22/12 7:08 PM
+ */
+public class AmenListFragmentActivity extends SherlockFragmentActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+setContentView(R.layout.);
+  }
+
+}
