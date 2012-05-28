83ddf2770e659a4c3324c4f11e7c4e3a04b5caef
Dirk Jäckel <github@dirk.jaeckel.name>
2012-05-28 03:03:54 +0200

    replaycing ( and ) in Objekt.getName as label

--- AmDroid/src/main/java/com/jaeckel/amenoid/AmenDetailFragmentActivity.java
+++ AmDroid/src/main/java/com/jaeckel/amenoid/AmenDetailFragmentActivity.java
@@ -222,7 +222,7 @@ public class AmenDetailFragmentActivity extends SherlockFragmentActivity {
 
     Double lat = getCurrentStatement().getObjekt().getLat();
     Double lng = getCurrentStatement().getObjekt().getLng();
-    String label = getCurrentStatement().getObjekt().getName();
+    String label = getCurrentStatement().getObjekt().getName().replaceAll("\\(", "").replaceAll("\\)", "");
 
 //    final String uriString = "geo:" + lat + ',' + lng + "?z=15";
     final String uriString = "https://maps.google.com/maps?q=" + lat + ',' + lng + "(" + label +")" + "&z=15";
