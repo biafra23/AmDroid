bf8e7d2c20053aa3d0d68f2b3cef8e8c70dd4df2
Dirk Jäckel <github@dirk.jaeckel.name>
2012-05-28 03:40:47 +0200

    AspectRatioImageView extends RemoteImageView

--- AmDroid/src/main/java/com/jaeckel/amenoid/util/AspectRatioImageView.java
+++ AmDroid/src/main/java/com/jaeckel/amenoid/util/AspectRatioImageView.java
@@ -5,23 +5,26 @@ package com.jaeckel.amenoid.util;
  * @date 4/19/12 10:28 PM
  */
 
+import com.github.ignition.core.widgets.RemoteImageView;
+
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.widget.ImageView;
 
-public class AspectRatioImageView extends ImageView {
+public class AspectRatioImageView extends RemoteImageView {
 
-    public AspectRatioImageView(Context context) {
-        super(context);
-    }
+  public AspectRatioImageView(Context context, String imageUrl, boolean autoLoad) {
+    super(context, imageUrl, autoLoad);
+  }
 
-    public AspectRatioImageView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+  public AspectRatioImageView(Context context, String imageUrl, Drawable progressDrawable, Drawable errorDrawable, boolean autoLoad) {
+    super(context, imageUrl, progressDrawable, errorDrawable, autoLoad);
+  }
 
-    public AspectRatioImageView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
+  public AspectRatioImageView(Context context, AttributeSet attributes) {
+    super(context, attributes);
+  }
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
