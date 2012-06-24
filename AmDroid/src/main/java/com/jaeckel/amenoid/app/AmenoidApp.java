cbf56a9f0cdbed9dfe89f40667afc3c5985f84a9
Dirk Jäckel <github@dirk.jaeckel.name>
2012-06-24 15:23:56 +0200

    location handling stuff left to ignition now

--- AmDroid/src/main/java/com/jaeckel/amenoid/app/AmenoidApp.java
+++ AmDroid/src/main/java/com/jaeckel/amenoid/app/AmenoidApp.java
@@ -3,7 +3,6 @@ package com.jaeckel.amenoid.app;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
-import java.util.List;
 
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
@@ -18,30 +17,21 @@ import com.jaeckel.amenoid.api.model.User;
 import com.jaeckel.amenoid.cwac.cache.SimpleWebImageCache;
 import com.jaeckel.amenoid.cwac.thumbnail.ThumbnailBus;
 import com.jaeckel.amenoid.cwac.thumbnail.ThumbnailMessage;
-import com.jaeckel.amenoid.db.AmenDBAdapter;
-import com.jaeckel.amenoid.db.AmenDao;
+
 import com.jaeckel.amenoid.util.Config;
 import com.jaeckel.amenoid.util.Log;
 
 import android.app.AlertDialog;
 import android.app.Application;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+
 import android.os.Handler;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
-import android.widget.Toast;
 import ch.boye.httpclientandroidlib.impl.client.DefaultHttpClient;
 
 /**
@@ -62,15 +52,6 @@ public class AmenoidApp extends Application {
   private String      authCookie;
   private AmenService service;
 
-  private Location         lastLocation;
-  private LocationManager  locationManager;
-  private LocationListener locationListener;
-  private PendingIntent    singleUpatePI;
-  private Criteria         criteria;
-  private AmenDBAdapter    dbAdapter;
-
-  private AmenDao amenDao;
-
   //CWAC
 
   private ThumbnailBus bus = new ThumbnailBus();
@@ -83,6 +64,9 @@ public class AmenoidApp extends Application {
 
   private SharedPreferences prefs;
   private DefaultHttpClient amenHttpClient;
+
+  private Location lastLocation;
+
   private Gson gson = new GsonBuilder()
     .registerTypeAdapter(Date.class, new DateSerializer())
     .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
@@ -133,22 +117,6 @@ public class AmenoidApp extends Application {
 //    handler = new Handler();
     Log.v(TAG, "onCreate");
 
-    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-    // Coarse accuracy is specified here to get the fastest possible result.
-    // The calling Activity will likely (or have already) request ongoing
-    // updates using the Fine location provider.
-    criteria = new Criteria();
-    criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-
-    // Construct the Pending Intent that will be broadcast by the oneshot
-    // location update.
-    Intent updateIntent = new Intent(SINGLE_LOCATION_UPDATE_ACTION);
-    singleUpatePI = PendingIntent.getBroadcast(this, 0, updateIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-    lastLocation = getLastBestLocation(100, 100);
-    Log.d(TAG, "lastLocation: " + lastLocation);
-
-
     final String authToken = readAuthTokenFromPrefs(prefs);
     final User me = readMeFromPrefs(prefs);
 
@@ -167,7 +135,6 @@ public class AmenoidApp extends Application {
     final String password = prefs.getString(Constants.PREFS_PASSWORD, null);
 
     if (authToken != null && me != null) {
-      Log.d(TAG, "configureAmenService: " + lastLocation);
       configureAmenService();
       service.init(authToken, me);
 
@@ -206,17 +173,8 @@ public class AmenoidApp extends Application {
   public boolean isSignedIn() {
 
     if (service != null && !TextUtils.isEmpty(service.getAuthToken())) {
-//      Log.v(TAG, "isSignedIn -> " + true);
       return true;
     }
-//    Log.v(TAG, "isSignedIn -> " + false);
-
-//    Log.v(TAG, "service: " + service);
-//    if (service != null) {
-//      Log.v(TAG, "service.getAuthToken(): " + service.getAuthToken());
-//
-//    }
-
     return false;
   }
 
@@ -264,17 +222,6 @@ public class AmenoidApp extends Application {
     return service;
   }
 
-  //CWAC
-//  void goBlooey(Throwable t) {
-//
-//    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//    Log.e(TAG, "Creating dialog");
-//    builder
-//      .setTitle(R.string.exception)
-//      .setMessage(t.getMessage())
-//      .setPositiveButton(R.string.ok, null)
-//      .show();
-//  }
 
   ThumbnailBus getBus() {
     return (bus);
@@ -317,8 +264,6 @@ public class AmenoidApp extends Application {
 
 
   public Location getLastLocation() {
-    lastLocation = getLastBestLocation(100, 100);
-    Log.d(TAG, "getLastLocation: " + lastLocation);
     return lastLocation;
   }
 
@@ -326,94 +271,6 @@ public class AmenoidApp extends Application {
     this.lastLocation = lastLocation;
   }
 
-  /**
-   * Returns the most accurate and timely previously detected location.
-   * Where the last result is beyond the specified maximum distance or
-   * latency a one-off location update is returned via the {@link LocationListener}
-   * specified in {@link }.
-   *
-   * @param minDistance Minimum distance before we require a location update.
-   * @param minTime     Minimum time required between location updates.
-   * @return The most accurate and / or timely previously detected location.
-   */
-  public Location getLastBestLocation(int minDistance, long minTime) {
-
-    Log.v(TAG, "getLastBestLocation");
-    Location bestResult = null;
-    float bestAccuracy = Float.MAX_VALUE;
-    long bestTime = Long.MIN_VALUE;
-
-    // Iterate through all the providers on the system, keeping
-    // note of the most accurate result within the acceptable time limit.
-    // If no result is found within maxTime, return the newest Location.
-    List<String> matchingProviders = locationManager.getAllProviders();
-    for (String provider : matchingProviders) {
-      Location location = locationManager.getLastKnownLocation(provider);
-      if (location != null) {
-        float accuracy = location.getAccuracy();
-        long time = location.getTime();
-
-        if ((time > minTime && accuracy < bestAccuracy)) {
-          bestResult = location;
-          bestAccuracy = accuracy;
-          bestTime = time;
-        } else if (time < minTime && bestAccuracy == Float.MAX_VALUE && time > bestTime) {
-          bestResult = location;
-          bestTime = time;
-        }
-      }
-    }
-
-    // If the best result is beyond the allowed time limit, or the accuracy of the
-    // best result is wider than the acceptable maximum distance, request a single update.
-    // This check simply implements the same conditions we set when requesting regular
-    // location updates every [minTime] and [minDistance].
-    if (locationListener != null && (bestTime < minTime || bestAccuracy > minDistance)) {
-      IntentFilter locIntentFilter = new IntentFilter(SINGLE_LOCATION_UPDATE_ACTION);
-      this.registerReceiver(singleUpdateReceiver, locIntentFilter);
-      locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10 * 60 * 1000, 50.0F, singleUpatePI);
-    }
-
-    return bestResult;
-  }
-
-  /**
-   * This {@link BroadcastReceiver} listens for a single location
-   * update before unregistering itself.
-   * The oneshot location update is returned via the {@link LocationListener}
-   * specified in {@link }.
-   */
-  protected BroadcastReceiver singleUpdateReceiver = new BroadcastReceiver() {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-
-      context.unregisterReceiver(singleUpdateReceiver);
-
-      String key = LocationManager.KEY_LOCATION_CHANGED;
-      Location location = (Location) intent.getExtras().get(key);
-
-      Toast.makeText(AmenoidApp.this, "Receve Location update: " + location, Toast.LENGTH_LONG).show();
-
-      if (locationListener != null && location != null)
-        locationListener.onLocationChanged(location);
-
-      locationManager.removeUpdates(singleUpatePI);
-    }
-  };
-
-  /**
-   * {@inheritDoc}
-   */
-  public void setChangedLocationListener(LocationListener l) {
-    locationListener = l;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public void cancel() {
-    locationManager.removeUpdates(singleUpatePI);
-  }
 
   public Typeface getAmenTypeThin() {
     return amenTypeThin;
@@ -423,11 +280,4 @@ public class AmenoidApp extends Application {
     return amenTypeBold;
   }
 
-//  public AmenDao getAmenDao() {
-//    return amenDao;
-//  }
-//
-//  public void setAmenDao(AmenDao amenDao) {
-//    this.amenDao = amenDao;
-//  }
 }
