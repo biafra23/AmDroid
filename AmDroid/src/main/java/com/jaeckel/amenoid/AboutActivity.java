36122efa10253f9ec769efca83d33b8464b4ec80
Dirk Jäckel <github@dirk.jaeckel.name>
2012-06-24 15:27:51 +0200

    back to sherlock, removed location stuff

--- AmDroid/src/main/java/com/jaeckel/amenoid/AboutActivity.java
+++ AmDroid/src/main/java/com/jaeckel/amenoid/AboutActivity.java
@@ -3,9 +3,17 @@ package com.jaeckel.amenoid;
 import java.text.SimpleDateFormat;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.github.ignition.location.annotations.IgnitedLocation;
+import com.github.ignition.location.annotations.IgnitedLocationActivity;
+import com.github.ignition.location.templates.OnIgnitedLocationChangedListener;
+import com.github.ignition.location.utils.IgnitedLocationSupport;
+import com.jaeckel.amenoid.util.Log;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -14,7 +22,10 @@ import android.widget.TextView;
  * Date: 11/5/11
  * Time: 8:13 PM
  */
-public class AboutActivity extends SherlockActivity {
+public class AboutActivity extends Activity {
+
+  private final static String TAG = AboutActivity.class.getSimpleName();
+
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
@@ -47,5 +58,47 @@ public class AboutActivity extends SherlockActivity {
 
     buildTimeTextView.setText("Built: " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Long.valueOf(timestampString)));
 
+//    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("ssh://biafra@biafra.diskordia.com:22/#biafra")));
+//    startActivity(new Intent(Intent.ACTION_PICK, Uri.parse("ssh://biafra@biafra.diskordia.com:22/#biafra")));
+
   }
+  // MUST BE OVERRIDDEN OR IGNITION LOCATION WON'T WORK!
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    // MUST BE OVERRIDDEN OR IGNITION LOCATION WON'T WORK!
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+
+
+      // If location is still to be determined, show a "wait for location" dialog
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == R.id.ign_loc_dialog_wait_for_fix) {
+            return IgnitedLocationSupport.createWaitForLocationDialog(this);
+        } else if (id == R.id.ign_loc_dialog_no_providers_enabled) {
+            return IgnitedLocationSupport.createNoProvidersEnabledDialog(this, true).create();
+        }
+        return super.onCreateDialog(id);
+    }
 }
